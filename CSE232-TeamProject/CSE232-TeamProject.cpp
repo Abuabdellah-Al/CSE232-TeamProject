b1214de404ecb1a9fb7903da999bfb22294afc93
Abu Abdellah <moamenahmedac@gmail.com>
2026-04-13 23:35:03 +0200

    -added json.hpp extension to deal with json in cpp -defined variables and a flag -created JSON data file for materials -defined functions to read and save to json. -defined function to add new material. -defined CreateTable func (needs edit) -add choose cross-section type -added choose exitsting material and create new

--- CSE232-TeamProject/CSE232-TeamProject.cpp
+++ CSE232-TeamProject/CSE232-TeamProject.cpp
@@ -1,20 +1,152 @@
 // CSE232-TeamProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <fstream>
+#define _USE_MATH_DEFINES
+#include "json.hpp"
+using json = nlohmann::json;
+using namespace std;
+
+//VARs
+bool is_Circular = false;
+json materials_json;
+int material_id = 0;
+//
+
+//read json file and store it in a json object
+void readjson(json& j, const string& filename)
+{
+	ifstream file(filename);
+	if (!file.is_open()) {
+		cerr << "Could not open the file!" << endl;
+	}
+	file >> j;
+	file.close();
+}
+
+//Save any json object to a file
+void savejson(json& j,const string& filename) {
+	std::ofstream writeFile(filename);
+	writeFile << std::setw(4) << j << std::endl;
+	writeFile.close();
+}
+
+
+void NewMaterial(json& j, const string& mname, const double& yield, const double& density)
+{
+	
+	json newMaterial = {
+		{"id",(j["materials"].size() + 1)},
+		{"name", mname},
+		{"sigma_yield", yield},
+		{"density", density}
+	};
+
+	// 3. Push it into the "materials" array
+	j["materials"].push_back(newMaterial);
+	savejson(j, "materials.json");
+}
+///////////////////////////////////////////////WILL BE EDITED TO BE ABLE TO DISPLAY OTHER TABLES
+void CreateTable() {
+	// Define column widths
+	const int idWidth = 4;
+	const int nameWidth = 20;
+	const int yieldWidth = 15;
+	const int densityWidth = 10;
+
+	// Print Header 
+	cout << " " << left << setw(idWidth) << "ID"
+		<< "| " << setw(nameWidth) << "Material"
+		<< "| " << setw(yieldWidth) << "Yield (Mpa)"
+		<< "| " << setw(densityWidth) << "Density(gm/cm^3)" << endl;
+
+	cout << " " << string(idWidth + nameWidth + yieldWidth + densityWidth + 6, '-') << endl;
+
+	for (const auto& m : materials_json["materials"]) {
+		int id = m["id"];
+		string name = m["name"];
+		double yield = m["sigma_yield"];
+		double density = m["density"];
+
+		cout << " " << left << setw(idWidth) << id
+			<< "| " << setw(nameWidth) << name
+			<< "| " << setw(yieldWidth) << yield
+			<< "| " << setw(densityWidth) << density << endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << R"(
+			 _____     ____   ____    ____   _______ 
+			|  __ \   / __ \ |  _ \  / __ \ |__   __|
+			| |__) | | |  | || |_) || |  | |   | |   
+			|  _  /  | |  | ||  _ < | |  | |   | |   
+			| | \ \  | |__| || |_) || |__| |   | |   
+			|_|  \_\  \____/ |____/  \____/    |_|   
+		    _____   ______   _____   _____    _____   _   _  
+		   |  __ \ |  ____| / ____| |_   _|  / ____| | \ | | 
+		   | |  | || |__   | (___     | |   | |  __  |  \| | 
+		   | |  | ||  __|   \___ \    | |   | | |_ | | |\  | 
+		   | |__| || |____  ____) |  _| |_  | |__| | | | \ | 
+		   |_____/ |______||_____/  |_____|  \_____| |_| \_|
+--------------------------------------------------------------------------------------------------------)";
+    cout << "\n \n Please choose the Cross-section type for the link\n for Circular (c) for Recatngular (r)....  ";
+
+	if (cin.get() == 'c') is_Circular = true;
+	
+	cout << "\n Please choose a material from this catalog.... \n";
+	//read the json file and print the materials in a table format
+	readjson(materials_json, "materials.json");
+
+	CreateTable();
+
+	string input;
+	cout << "\nType the material ID to choose, or to add a new press (n): ";
+
+	while (true)
+	{
+		cin >> input;
+
+		// 1. Check for the 'new' command first
+		if (input == "n" || input == "N")
+		{
+			string name;
+			double sigma_yield, density;
+
+			cout << "\n Please enter the material name..."; cin >> name;
+			cout << "\n material yield strength..."; cin >> sigma_yield;
+			cout << "\n material density..."; cin >> density;
+			NewMaterial(materials_json, name, sigma_yield, density);
+			CreateTable();
+		}
+		else
+		{
+			// 2. If it's not 'n', try to parse it as an ID
+			try
+			{
+				int id = stoi(input);
+
+
+				if (id > 0 && id <= materials_json["materials"].size())
+				{
+					material_id = id;
+					break;
+				}
+				else
+				{
+					cout << "\nID out of range. Please try again.\n";
+				}
+			}
+			// 3. Catch errors if they type random letters like "abc"
+			catch (const invalid_argument&)
+			{
+				cout << "\nInvalid input. Please enter a valid number or 'n'.\n";
+			}
+		}
+	}
+
+	cout << "\nYou have selected material: " << materials_json["materials"][material_id - 1]["name"] << endl;
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
